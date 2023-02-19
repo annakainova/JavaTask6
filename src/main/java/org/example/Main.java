a6183e2354ef1a27d93241fb17657f963644b7af
Anna Kainova <annakainova88@gmail.com>
2023-02-19 12:49:28 +0100

    first part

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import ru.netology.task6.services.VacationSchedulerService;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/ru/netology/task6/services/VacationSchedulerServiceTest.java
@@ -0,0 +1,27 @@
+package ru.netology.task6.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VacationSchedulerServiceTest {
+
+    @Test
+    public void testLowIncome() {
+        VacationSchedulerService service = new VacationSchedulerService();
+
+        int expected = 3;
+        int actual = service.calculate(10,3,20);
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testHighIncome() {
+        VacationSchedulerService service = new VacationSchedulerService();
+
+        int expected = 2;
+        int actual = service.calculate(100,60,150);
+
+        Assertions.assertEquals(actual, expected);
+    }
+}
